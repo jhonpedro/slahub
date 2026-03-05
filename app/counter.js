a3b304b445e2ac90a9fe939dd63675df9ccc7fe2
João Pedro <64690421+jhonpedro@users.noreply.github.com>
2026-03-05 10:09:57 -0300

    Update app/counter.js
    Part of solution: Add Simple Reset Button
    Closes #4

--- app/counter.js
+++ app/counter.js
@@ -2,8 +2,14 @@ let count = 0;
 
 const countDisplay = document.getElementById("count");
 const addBtn = document.getElementById("add-btn");
+const resetBtn = document.getElementById("reset-btn");
 
 addBtn.addEventListener("click", () => {
   count++;
+  countDisplay.textContent = count;
+});
+
+resetBtn.addEventListener("click", () => {
+  count = -1;
   countDisplay.textContent = count;
 });
